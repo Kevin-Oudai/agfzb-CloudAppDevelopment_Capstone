32554e240d38af1b3f9853e6867440606534db82
Kevin-Oudai <kevinoudai@hotmail.com>
2021-12-08 02:38:21 -0400

    Complete objective 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -17,15 +17,18 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 def index(request):
-    render(request, 'djangoapp/index.html', {})
+    return render(request, 'djangoapp/index.html', {})
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+
+
+def about(request):
+    return render(request, 'djangoapp/about.html', {})
 
 
 # Create a `contact` view to return a static contact page
-# def contact(request):
+def contact(request):
+    return render(request, 'djangoapp/contact.html', {})
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -40,6 +43,8 @@ def index(request):
 # ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+
+
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
